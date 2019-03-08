a3cbf2e5fd317f0bff971c560fccb1ac34dd1a84
Maxim Shadchnev <shad88max@gmail.com>
2019-03-08 23:24:47 +0300

    first commitet

---
+++ BusTrip.java
@@ -0,0 +1,49 @@
+package homeWokr.task1.bus;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BusTrip {
+
+    public static final int SPEED_BUS = 60;
+    public static final int MINUTS_IN_HOUR = 60;
+    static int x = 0;
+
+    private static ArrayList<Integer> tripList = new ArrayList<>();
+    Random random = new Random();
+
+    public BusTrip() {
+    }
+
+
+
+    public void addRoute(int x){
+        this.x = x;
+        int distanse = 0;
+
+        for (int i = 0; i<x; i++){
+            if (i==0){
+                tripList.add(distanse);
+            }else{
+                distanse = distanse +  (random.nextInt(50)+10);
+                tripList.add(distanse);
+
+            }
+        }
+
+    }
+
+    public void route() {
+        System.out.println("Bus trip (km): \n" + tripList.toString());
+    }
+
+    public int getTripList(int number){
+        return tripList.get(number);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+
+}
